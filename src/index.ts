3f65a2eaa3108e34004ec55bf9e7fb6b4bcf6fbd
Robbie <robbiethewadley@gmail.com>
2022-09-19 13:17:45 -0600

    respond to dms

--- src/index.ts
+++ src/index.ts
@@ -36,23 +36,23 @@ const sendMessage = async (
   let botUserId = context.botUserId;
 
   // get the 10 most recent messages in the channel
-  const history =
+  const prompt =
     (
       await client.conversations.history({
         channel: channelId,
         limit: randomInt(2, 7),
       })
     ).messages
-      ?.flatMap((m) => (m.text ? ["msg_" + m.user + ": " + m.text] : []))
+      ?.flatMap((m) => (m.text ? ["msg_" + m.user + "- " + m.text] : []))
       .reverse()
       .join("\n") +
-    "\ngenerate a new moderately insulting message:\nmsg" +
+    "\ngenerate an insulting response:\nmsg" +
     botUserId +
-    ":";
+    "-";
 
   const completion = await openai.createCompletion({
     model: "text-curie-001",
-    prompt: "" + history,
+    prompt: prompt,
     max_tokens: 250,
     stop: "msg",
     temperature: 0.7,
@@ -64,14 +64,14 @@ const sendMessage = async (
 
   const regex = /<[@|+][0-9A-Za-z]+>/g;
   const newText = text.replace(regex, (match) => {
-    if (history.includes(match) && match !== "<@" + botUserId + ">") {
+    if (prompt.includes(match) && match !== "<@" + botUserId + ">") {
       return match;
     } else {
       return "";
     }
   });
 
-  console.log(history + newText);
+  console.log(prompt + newText);
   if (newText)
     try {
       await say({
@@ -105,15 +105,9 @@ app.event("app_mention", async ({ event, context, client, say }) => {
 });
 
 // subscribe to 'message' event in your App config
-// app.event("message", async ({ event, context, client, say }) => {
-//   console.log("MESSAGE");
-//   if (
-//     // not from bot
-//     event.channel === lastChannelId &&
-//     randomChance(50) &&
-//     // most recent ping was in the last 2 minutes
-//     new Date().getTime() - dateOfLastPing.getTime() < 2 * 60 * 1000
-//   ) {
-//     sendMessage(event.channel, context, client, say);
-//   }
-// });
+app.event("message", async ({ event, context, client, say }) => {
+  // if is in DM
+  if (event.channel_type === "im") {
+    sendMessage(event.channel, context, client, say);
+  }
+});
