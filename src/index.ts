2a49da0b4efa573ed65c48e331d9c7d11bbf605d
Robbie <robbiethewadley@gmail.com>
2022-12-12 12:19:10 -0700

    decrease probability

--- src/index.ts
+++ src/index.ts
@@ -168,8 +168,8 @@ app.event("message", async ({ event, context, client, say }) => {
     if (
       // if the message includes the word "lunch", respond
       mostRecent.messages?.[0].text?.toLowerCase().includes("lunch") ||
-      // or a 10% chance
-      Math.random() < 0.1
+      // or a 5% chance
+      Math.random() < 0.05
     ) {
       console.log("lunch");
       sendMessage(event.channel, context, client, say);
